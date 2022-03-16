ca3fa8a323a1054c9f048e17d04410ae4fce33ed
Burak Sırma <hsirmaa@gmail.com>
2022-03-16 21:37:05 +0300

    Fix config paths

--- config.fish
+++ config.fish
@@ -1,5 +1,4 @@
-set -gxp PATH /opt/homebrew/bin $HOME/go/bin
-set -gx GOBIN $HOME/go/bin
+set -gxp PATH /opt/homebrew/bin
 set -gx EDITOR nvim
 
 set -g fish_color_option "E6B450"
@@ -9,10 +8,7 @@ alias vim "nvim"
 alias ls "exa"
 alias ll "exa -lhg"
 
-# don't show any greetings
 set fish_greeting ""
 
 # brew install jump, https://github.com/gsamokovarov/jump
 status --is-interactive; and source (jump shell fish | psub)
-
-set -g fish_user_paths "/usr/local/opt/python@3.9/libexec/bin" $fish_user_paths
