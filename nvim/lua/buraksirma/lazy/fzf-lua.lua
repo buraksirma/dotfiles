b418d77a1de3bd7453283e2411a935daa47d863b
Burak Sırma <hsirmaa@gmail.com>
2024-12-18 23:39:57 +0300

    bring some telescope behaviour back

--- nvim/lua/buraksirma/lazy/fzf-lua.lua
+++ nvim/lua/buraksirma/lazy/fzf-lua.lua
@@ -18,7 +18,36 @@ return {
 	},
 	config = function()
 		local fzf = require("fzf-lua")
-		fzf.setup({})
+		local actions = fzf.actions
+
+		fzf.setup({
+			lsp = {
+				jump_to_single_result = true,
+				jump_to_single_result_action = actions.file_edit,
+			},
+			keymap = {
+				builtin = {
+					true,
+					["<C-d>"] = "preview-page-down",
+					["<C-u>"] = "preview-page-up",
+				},
+				fzf = {
+					true,
+					["ctrl-d"] = "preview-page-down",
+					["ctrl-u"] = "preview-page-up",
+					["ctrl-q"] = "select-all+accept",
+				},
+			},
+			actions = {
+				files = {
+					["enter"] = actions.file_edit_or_qf,
+					["ctrl-x"] = actions.file_split,
+					["ctrl-v"] = actions.file_vsplit,
+					["ctrl-t"] = actions.file_tabedit,
+					["alt-q"] = actions.file_sel_to_qf,
+				},
+			},
+		})
 
 		vim.keymap.set("n", "<C-p>", fzf.files, { noremap = true })
 		vim.keymap.set("v", "<C-p>", function()
