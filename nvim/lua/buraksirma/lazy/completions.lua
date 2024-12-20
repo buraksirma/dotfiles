8391ff88a8c1c876dc0bce46237b30c025c2bbee
Burak Sırma <hsirmaa@gmail.com>
2024-12-20 22:41:44 +0300

    fix blink & luadev integration

--- nvim/lua/buraksirma/lazy/completions.lua
+++ nvim/lua/buraksirma/lazy/completions.lua
@@ -17,18 +17,18 @@ return {
 			},
 			sources = {
 				default = { "lazydev", "lsp", "path", "snippets", "buffer" },
+				providers = {
+					lazydev = {
+						name = "LazyDev",
+						module = "lazydev.integrations.blink",
+						-- make lazydev completions top priority (see `:h blink.cmp`)
+						score_offset = 100,
+					},
+				},
 			},
 			completion = {
 				menu = {
 					border = "single",
-				},
-			},
-			providers = {
-				lazydev = {
-					name = "LazyDev",
-					module = "lazydev.integrations.blink",
-					-- make lazydev completions top priority (see `:h blink.cmp`)
-					score_offset = 100,
 				},
 			},
 		},
